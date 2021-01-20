4add0d0886b89308cb05d5cf9d4fd369cf2286a8
Mert ESER <merteser@outlook.com>
2021-01-20 23:51:12 +0300

    20.01.2020 Ders sonu

---
+++ Collections/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = new string[] { "Mert", "Azra", "Hatice", "Mevlüt" }; //string array
+
+            List<string> myFamily = new List<string>() {
+                "Mert","Azra","Hatice","Mevlüt"
+            };
+
+            myFamily.Add("Bilge");
+
+            foreach (var item in myFamily)
+            {
+                Console.WriteLine(item);
+            }
+
+
+        }
+    }
+
+    class Product
+    {
+
+    }
+}
---
+++ OOP1/ProductManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OOP1
+{
+    public class ProductManager
+    {
+        public ProductManager()
+        {
+
+        }
+
+        public void Add(Product product)
+        {
+            Console.WriteLine(product.ProductName + " eklendi.");
+        }
+
+        public void Update(Product product)
+        {
+            Console.WriteLine(product.ProductName + " güncellendi.");
+        }
+
+        public int Topla(int number1,int number2)
+        {
+            return number1 + number2;
+        }
+    }
+}
